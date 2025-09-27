4626387b7b785152f48bce250c3b33b65b2ff02a
DimasAndrian2327240089 <andriandimas490@gmail.com>
2025-09-27 12:37:16 +0700

    Pertemuan5

--- toko-online/routes/index.js
+++ toko-online/routes/index.js
@@ -1,11 +1,31 @@
 var express = require("express");
 var router = express.Router();
-var products = require("../data/product.json");
+var product= require("../data/product.json");
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-  res.render("index", { 
-    title: "Tokoh Online Sederhana", products: products 
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "Toko Online Sederhana",
+    product: product,
+    query: null, //biar bisa dipakai di view
+  });
+});
+
+/* GET search page. */
+router.get("/search", function (req, res, next) {
+  const q = req.query.q ? req.query.q.toLowerCase() : "";
+
+  let filteredproduct;
+  if (!q) {
+    filteredproduct = product; // jika query kosong tampilkan semua
+  } else {
+    filteredproduct = product.filter((p) => p.name.toLowerCase().includes(q));
+  }
+
+  res.render("index", {
+    title: "Hasil Pencarian",
+    product: filteredproduct,
+    query: q,
   });
 });
 
